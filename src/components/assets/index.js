96865902c6da5426f377baac50c9414b39f1a43e
climax-solution <blocklimax@gmail.com>
2021-12-09 15:36:29 +0300

    add price popup

--- src/components/assets/index.js
+++ src/components/assets/index.js
@@ -1,29 +1,38 @@
 import { useEffect, useState } from "react";
-import { useWeb3React } from '@web3-react/core'
 import Swal from "sweetalert2";
+import withReactContent from 'sweetalert2-react-content'
+import { NotificationManager } from "react-notifications";
 import getWeb3NoAccount from "../../utils/web3";
 import InnerNav from '../layout/marketplace-nav';
 import Token from "../../ABI/Token.json";
 import Robinhood from "../../ABI/Mint.json";
 import Marketplace from "../../ABI/Marketplace.json";
 import address from "../../config/address.json";
-import { IpfsStorage } from "../../IPFSStorage/ipfs";
+import empty from "../../Assets/empty.png";
+
+import ScreenLoading from "../Loading/screenLoading";
+import ItemLoading from "../Loading/itemLoading";
+
 const { marketplace_address, nft_address, token_address } = address;
 
 const MyAssets = () => {
-    const {account} = useWeb3React();
-    const [web3, setWeb3] = useState({});
-    const [token, setToken] = useState({});
-    const [robinHood, setNFT] = useState({});
-    const [marketplace, setMarketplace] = useState({});
+    const [web3, setWeb3] = useState(null);
+    const [token, setToken] = useState(null);
+    const [robinHood, setNFT] = useState(null);
+    const [marketplace, setMarketplace] = useState(null);
     const [assets, setAssets] = useState([]);
+    const [account, setAccount] = useState();
+    const [isScreen, setScreenLoading] = useState(false);
+    const [isItem, setItemLoading] = useState(true);
 
     useEffect(async () => {
         const _web3 = await getWeb3NoAccount();
         const _robinHood = new _web3.eth.Contract(Robinhood, nft_address);
         const _token = new _web3.eth.Contract(Token, token_address);
         const _marketplace = new _web3.eth.Contract(Marketplace, marketplace_address);
-
+        const _account = await _web3.eth.getAccounts();
+        
+        setAccount(_account[0]);
         setWeb3(_web3);
         setToken(_token);
         setNFT(_robinHood);
@@ -31,9 +40,16 @@ const MyAssets = () => {
 
     },[]);
 
+    useEffect(async() => {
+        if (!web3 || !robinHood || !marketplace || !account ) return;
+        await getPersonalNFT();
+    },[web3, robinHood, marketplace, account])
 
     const putOnSale = async (id) => {
-        await Swal.fire({
+        const accounts = await web3.eth.getAccounts();
+        const account = accounts[0];
+        const priceSwal = withReactContent(Swal);
+        await priceSwal.fire({
           title: '<span style="font-size: 22px">PLEASE ENTER PRICE</span>',
           input: 'number',
           width: 350,
@@ -48,20 +64,24 @@ const MyAssets = () => {
           confirmButtonText: 'OK',
           showLoaderOnConfirm: true,
           allowOutsideClick: () => !Swal.isLoading()
-        }).on('receipt',async(result) => {
+        })
+        .then(async(result) => {
           if (result.isConfirmed) {
-  
+            setScreenLoading(true);
             try {
-              const photoPrice = web3.utils.toWei((result.value).toString(), 'ether');
+              const photoPrice = web3.utils.toWei((result.value).toString(), 'gwei');
               await robinHood.methods.approve(marketplace_address, id).send({from : account });
               await token.methods.approve(marketplace_address, photoPrice).send({ from: account });
-              await marketplace.methods.openTrade(id, photoPrice / 20).send({ from: account })
+              await marketplace.methods.openTrade(id, photoPrice / 10).send({ from: account })
               .then( async(res) => {
-                // NotificationManager.success("Success");
-                await this.getPersonalNFT();
+                NotificationManager.success("Success");
+                await getPersonalNFT();
+                setScreenLoading(false);
               })
             } catch(err) {
-            //   NotificationManager.error("Failed");
+                console.log(err);
+              NotificationManager.error("Failed");
+                setScreenLoading(false);
             }
           }
         })
@@ -69,25 +89,33 @@ const MyAssets = () => {
     }
 
     const cancelOnSale = async (id) => {
+        setScreenLoading(true);
         try {
-        const photo = await marketplace.methods.getNFTItem(id).call();
-        const buyAmount = photo.marketData.price;
-        await token.methods.approve(marketplace_address, buyAmount).send({ from: account });
-        await marketplace.methods.cancelTrade(id).send({ from: account, value: buyAmount / 20 })
-        .on('receipt',async(result) => {
-            // NotificationManager.success("Success");
-            await this.getPersonalNFT();
-        });
+            const accounts = await web3.eth.getAccounts();
+            const account = accounts[0];
+            const photo = await marketplace.methods.getNFTItem(id).call();
+            const buyAmount = photo.marketInfo.price;
+            await token.methods.approve(marketplace_address, buyAmount).send({ from: account });
+            await marketplace.methods.closeTrade(id, buyAmount / 10).send({ from: account})
+            .on('receipt',async(result) => {
+                NotificationManager.success("Success");
+                setScreenLoading(false);
+                await getPersonalNFT();                
+            });
         } catch(err) {
-            // NotificationManager.error("Failed");
+            setScreenLoading(false);
+            NotificationManager.error("Failed");
         }
     }
 
     const getPersonalNFT = async() => {
-        let list = await marketplace.methods.getAllPhotos().call();
+        setItemLoading(true);
+        const accounts = await web3.eth.getAccounts();
+        const account = accounts[0];
+        let list = await marketplace.methods.getAllNFTs().call();
         list = list.filter(item => item.baseInfo.owner == account);
         const finalResult = await Promise.all(list.map(async (item) => {
-            const response = await fetch(`${process.env.REACT_APP_IPFS}/ipfs/${item.baseInfo.tokenURI}`);
+            const response = await fetch(`http://localhost:8080/ipfs/${item.baseInfo.tokenURI}`);
             if(!response.ok)
                 throw new Error(response.statusText);
   
@@ -97,55 +125,61 @@ const MyAssets = () => {
         }) );
 
         setAssets(finalResult);
+        setItemLoading(false);
     }
 
     return (
-        <section className="live-auctions ">
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="col-xl-12 col-lg-12 main-image-ss">
-                        <div className="image-back-main ">
-                            <div className="nav-nmn pt-5">
-                                <InnerNav active="personal"/>
-                                <div className="row">
-                                    <div className="col-md-11 m-auto">
-                                        <div className="main-outer-content">
-                                            <div className="row">
-                                                
-                                                {
-                                                    assets.map(item => {
-                                                        return (
-                                                            <div className="col-lg-3 col-md-4 col-sm-6 col-12 mb-3">
-                                                                <div className="ITEM-CARD">
-                                                                    <div className="upper-div-img">
-                                                                        <img alt="" src={`${process.env.REACT_APP_IPFS}/ipfs/${item.image}`} className="img-fluid " />
-                                                                    </div>
-
-
-                                                                    <div className="lower-text-ares">
-
-                                                                        <h4>{item.name}</h4>
-                                                                        <div className="price">
-                                                                            <h6 className="mr-5">Price <span>{web3.utils.fromWei(item.marketInfo.price, 'gwei')}</span></h6>
-                                                                            <h6>1 of {item.copyNumber}</h6>
+        <>
+            { isScreen && <ScreenLoading/> }
+            <section className="live-auctions ">
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-xl-12 col-lg-12 main-image-ss">
+                            <div className="image-back-main ">
+                                <div className="nav-nmn pt-5">
+                                    <InnerNav active="personal"/>
+                                    <div className="row" style={{minHeight: '300px'}}>
+                                        <div className="col-md-11 m-auto">
+                                            <div className="main-outer-content">
+                                                <div className="row">
+                                                    { isItem && <ItemLoading/>}
+                                                    {
+                                                        !isItem &&
+                                                        assets.map((item, idx) => {
+                                                            return (
+                                                                <div className="col-lg-3 col-md-4 col-sm-6 col-12 mb-3" key={idx}>
+                                                                    <div className="ITEM-CARD">
+                                                                        <div className="upper-div-img">
+                                                                            <img alt="" src={`http://localhost:8080/ipfs/${item.image}`} className="img-fluid " />
                                                                         </div>
-                                                                        <div className="buttonss">
-                                                                            {
-                                                                                item.marketInfo.marketStatus ?
-                                                                                    <button onClick={() => putOnSale(item.baseInfo.tokenID)}>Sell</button>
-                                                                                : <button onClick={() => cancelOnSale(item.baseInfo.tokenID)}>Cancel Sell</button>
-                                                                            }
+
+
+                                                                        <div className="lower-text-ares">
+
+                                                                            <h4>{item.name}</h4>
+                                                                            <div className="price">
+                                                                                <h6 className="mr-5">Price <span>{web3.utils.fromWei(item.marketInfo.price, 'gwei')}</span></h6>
+                                                                                <h6>1 of {item.copyNumber}</h6>
+                                                                            </div>
+                                                                            <div className="buttonss">
+                                                                                {
+                                                                                    !item.marketInfo.marketStatus ?
+                                                                                        <button onClick={() => putOnSale(item.baseInfo.tokenID)}>Sell</button>
+                                                                                    : <button onClick={() => cancelOnSale(item.baseInfo.tokenID)}>Cancel Sell</button>
+                                                                                }
+                                                                            </div>
                                                                         </div>
                                                                     </div>
                                                                 </div>
-                                                            </div>
-                                                        )
-                                                    })
-                                                }
+                                                            )
+                                                        })
+                                                    }
 
-                                                {
-                                                    !assets.length && <h3>Empty</h3>
-                                                }
+                                                    {
+                                                        !assets.length && !isItem &&
+                                                        <div className="text-center w-100"><img src={empty}/></div>
+                                                    }
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
@@ -154,8 +188,8 @@ const MyAssets = () => {
                         </div>
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        </>
     )
 }
 
